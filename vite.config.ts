e8e1c7a150e3a46fe8d76664d9b0876c54f2e1f7
alexzulq <32060806+alexzulq@users.noreply.github.com>
2026-03-03 12:17:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       // ADD THIS LINE BELOW
-      base: '/Solar/', 
+      base: '/pv-weighted-calc/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
